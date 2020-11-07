a87a60d4f88bc52648ca30f020b7d8e9d5dabcb7
redxzeta <xzetanzero@gmail.com>
2020-11-07 08:12:14 -0800

    flask api
    our backend guy quit

---
+++ app.py
@@ -0,0 +1,4 @@
+from flask import *
+from flask_cors import *
+
+print('hello world')
---
+++ tests/test_liquid_app_flask.py
@@ -0,0 +1,5 @@
+from liquid_app_flask import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
